360a1e61c87628b2e7a0b23ca7949afd2e37e6d4
lars buerger <larsbuerger1@gmail.com>
2022-11-27 14:46:58 +0100

    fda

--- semester5/dbsys2/three/aufgabe3_3.sql
+++ semester5/dbsys2/three/aufgabe3_3.sql
@@ -1,4 +1,8 @@
+--3
+
 select s.matrikelnummer, s.name, s.getNotenschnitt() as Notenschnitt
 from StudierenderTab s;
 
 select p.studierender.name, p.note from PruefungsergebnisTab p;
+
+select * from StudierenderTab;
